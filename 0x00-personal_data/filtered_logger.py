0371bc5def827cb2a93556709d103c9617a2a27d
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-02-12 04:52:58 -0500

    A function that returns the log message with certain fields obfuscated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,7 +6,8 @@ Arguments:
     fields: a list of strings representing all fields to obfuscate
     redaction: a string representing by what the field will be obfuscated
     message: a string representing the log line
-    separator: a string representing by which character is separating all fields in the log line (message)
+    separator: a string representing by which character is
+    separating all fields in the log line (message)
 uses a regex to replace occurrences of certain field values.
 """
 
