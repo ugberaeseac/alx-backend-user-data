9ff8702dd1e1a06c690fe8a3592d3159137ebbac
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-02-13 18:21:39 -0500

    define routes that need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-
+Manage API authentication
+    create Auth class
+    class is the template for all authentication system you will implement.
 """
 
 
@@ -10,23 +12,34 @@ from typing import List, TypeVar
 
 class Auth():
     """
-
+    Auth class
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-
+        requires authentication
         """
-        return False
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        if path[-1] != '/':
+            path += '/'
+
+        if excluded_paths[-1] != '/':
+            excluded_paths += '/'
+
+        if path in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """
-
+        authorization header
         """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
-
+        current user
         """
         return None
