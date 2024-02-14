9985d7a7a324cc6b17ac134c848c7400f457ad77
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-02-14 17:24:18 -0500

    Basic Auth - Base64 header extraction

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,3 +13,16 @@ class BasicAuth(Auth):
     Basic Authentication
     inherits from Auth class
     """
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+        returns Base64 section of the authorization header
+        """
+        if authorization_header is None or not isinstance(
+                authorization_header, str):
+            return None
+        if authorization_header[:6] != "Basic ":
+            return None
+
+        return authorization_header[6:]
