6a6403a254e901b4d90be8d2a3f31369e2a7dce2
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-02-17 18:16:43 -0500

    create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -6,6 +6,7 @@ session authentication
 
 
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
@@ -13,4 +14,15 @@ class SessionAuth(Auth):
     session authentication
     class SessionAuth inherits from Auth class
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ 
+        creates a session id for a user
+        using its user_id
+        """
+        if user_id is None or type(user_id) is not str:
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
