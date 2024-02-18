b7c2aa0dd2605cb2a8abd405a632ce5180d5ec9f
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-02-18 16:53:31 -0500

    update before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -36,12 +36,14 @@ def before_request() -> str:
         return
     excluded_path = ['/api/v1/status/',
                      '/api/v1/unauthorized/',
-                     '/api/v1/forbidden/']
+                     '/api/v1/forbidden/'
+                     '/api/v1/auth_session/login']
 
     if not auth.require_auth(request.path, excluded_path):
         return
 
-    if auth.authorization_header(request) is None:
+    if not auth.authorization_header(request) and not auth.session_cookie(
+            request):
         abort(401)
 
     if auth.current_user(request) is None:
