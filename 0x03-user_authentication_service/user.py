4ef435805b89e2c24864a364cb0acfebfbbae0dc
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-02-20 16:10:39 -0500

    User model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+SQLalchemy user model with attributes
+    id: integer primary key
+    email: a non-nullable string
+    hashed_password: a non-nullable string
+    session_id: a nullable string
+    reset_token: a nullable string
+"""
+
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    SQLalchemy model User for a database table
+    users
+    """
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key= True)
+    email = Column(String(250), nullable=False)
+    hashed_passord = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
