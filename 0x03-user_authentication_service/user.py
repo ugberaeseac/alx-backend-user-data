f2b38257031253eb711b4728015bbc78f4ffab7e
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-02-20 16:14:59 -0500

    Fixed bug

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -24,5 +24,5 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_passord = Column(String(250), nullable=False)
-    session_id = Column(String(250))
-    reset_token = Column(String(250))
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
