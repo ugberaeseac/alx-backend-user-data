9cd9e1bfa16ccf5deecb947ef0292238c97c4ad1
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-02-21 16:05:58 -0500

    Implement login method to the /session route to log in an authorised user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,8 +4,9 @@ Basic Flask App
 """
 
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
+from db import DB
 
 
 app = Flask(__name__)
@@ -33,6 +34,25 @@ def register():
         return jsonify({"email": email, "message": "user created"}), 200
     except Exception:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """
+    Implement a login route
+    respond with a 401 HTTP status if login incorrect
+    otherwise create a new session ID and store as a cookie
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user = AUTH.valid_login(email, password)
+    if user:
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
+    abort(401)
 
 
 if __name__ == '__main__':
