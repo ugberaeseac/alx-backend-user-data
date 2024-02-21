a99263db774de446089dc9697fad23036577542e
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-02-21 09:32:38 -0500

    register a new user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,8 +6,33 @@ a salted hash of the input password in bytes
 """
 
 
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 import bcrypt
 salt = bcrypt.gensalt()
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        register a new user
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashed_pwd = _hash_password(password)
+            new_user = self._db.add_user(email, hashed_pwd)
+            return new_user
+        else:
+            msg = "User {} already exists"
+            raise ValueError(msg.format(email))
 
 
 def _hash_password(password: str) -> bytes:
