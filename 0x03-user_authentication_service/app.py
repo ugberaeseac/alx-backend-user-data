9c14b465aeddbd0c924be3d3b7e150463f909ce8
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-02-21 13:05:17 -0500

    implemented an end-point to register a user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,10 +4,12 @@ Basic Flask App
 """
 
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -17,6 +19,19 @@ def home():
     returns a JSON
     """
     return jsonify({"message": "Bienvenue"})
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register():
+    """
+    end-point to register a user
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == '__main__':
