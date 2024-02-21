65d275b8e55eb4a5cd593916829b5183af46ea02
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-02-21 14:49:46 -0500

    implement method that generates a session_id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -47,6 +47,19 @@ class Auth:
         except NoResultFound:
             return False
 
+    def create_session(self, email: str) -> str:
+        """
+        generate a new UUID and store it in the
+        database's user session_id
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
+
 
 def _hash_password(password: str) -> bytes:
     """
