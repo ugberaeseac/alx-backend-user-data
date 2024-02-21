cad44caa61cb0c4a43237e06ccc477cc7cefd19e
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-02-21 16:24:56 -0500

    find user by session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -60,7 +60,7 @@ class Auth:
         except NoResultFound:
             return None
 
-    def get_user_from_session_id(session_id: str) -> User:
+    def get_user_from_session_id(self, session_id: str) -> User:
         """
         return the corresponding User using the session_id
         """
