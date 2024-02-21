f93d679434483e70f494248898308704b08cfea1
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-02-21 18:15:06 -0500

    generate a reset password token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -83,7 +83,7 @@ class Auth:
             return None
         return None
 
-    def get_reset_password_token(email: str) -> str:
+    def get_reset_password_token(self, email: str) -> str:
         """
         Generate a reset password token
         """
