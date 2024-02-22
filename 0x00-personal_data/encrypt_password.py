e55390421e21303f19fd3a2c5df66ef443045f51
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-02-22 16:07:42 -0500

    debugging

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,12 +8,19 @@ returns a salted hashed password in byte string
 import bcrypt
 
 
-salt = bcrypt.gensalt()
-
-
 def hash_password(password: str) -> bytes:
     """
     hash the password
     """
-    hashed_pwd = bcrypt.hashpw(password.encode(), salt)
+    hashed_pwd = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     return hashed_pwd
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    check if password is valid
+    returns true if valid else false
+    """
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+        return True
+    return False
